c4ef72148c9f3427059e705c0be1acaf36133910
Gaio Moreira <154755555+ErasedSin@users.noreply.github.com>
2026-01-29 19:10:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+const carousel = document.getElementById("carousel");
+const label = document.getElementById("label");
+
+let items = [];
+let index = 0;
+
+fetch("links.json")
+  .then(res => res.json())
+  .then(data => {
+    items = data.items;
+    render();
+  });
+
+function render() {
+  carousel.innerHTML = "";
+
+  items.forEach((item, i) => {
+    const div = document.createElement("div");
+    div.className = "circle" + (i === index ? " active" : "");
+    div.innerHTML = `<img src="${item.image}" alt="${item.name}">`;
+
+    div.onclick = () => {
+      if (i === index) {
+        window.open(item.url, "_blank");
+      } else {
+        index = i;
+        render();
+      }
+    };
+
+    carousel.appendChild(div);
+  });
+
+  label.textContent = items[index].name;
+}
 
